5a27f5ca1c870f60a81b9a31ade40cb56437490c
Nandan Jha <nandanjha.dev@gmail.com>
2026-05-18 11:01:58 +0530

    Day 3: Added POST and GET notes API

--- day3/server.js
+++ day3/server.js
@@ -1,18 +1,31 @@
 const express = require("express")
 
-const app = express()
+const app = express()//the server we created by express here is not capable so much that it can read req.body data coming from client to server so we use middlewear to parse json method 
+
+// 🔥 Middleware to parse JSON
+app.use(express.json())
 
 const notes = [
-    {
-        title: "test title 1",
-        description: "description title 1",
-    },
-    {
-        title: "test title 2",
-        description: "description title 2",
-    }
+    // {
+    //     title: "test title 1",
+    //     description: "description title 1",
+    // },
+    // {
+    //     title: "test title 2",
+    //     description: "description title 2",
+    // }
 ]
 
-app.listen(3000,()=>{
-    console.log("server is running")
+app.post("/notes",(req,res)=>{
+    console.log(req.body)// to read req.body data we use app.use(express.json())
+    notes.push(req.body)
+    res.send("notes created")
 })
+
+app.get("/notes",(req,res)=>{
+    res.send(notes)
+})
+
+
+app.listen(3000)
+
