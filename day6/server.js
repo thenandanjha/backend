9229cd1a6cd529cf5c1e2fec94182a46f98ac67c
Nandan Jha <nandanjha.dev@gmail.com>
2026-05-23 15:24:18 +0530

    code update

--- day6/server.js
+++ day6/server.js
@@ -4,7 +4,8 @@ const app = require("./src/app")
 const mongoose = require("mongoose")
 
 function connectToDb(){
-    mongoose.connect("mongodb+srv://nandanjhadev_db_user:qksVm30t1S8ukHA3@cluster0.rcpcwfg.mongodb.net/day-6")
+    // mongoose.connect("mongodb+srv://nandanjhadev_db_user:qksVm30t1S8ukHA3@cluster0.rcpcwfg.mongodb.net/day-6")
+    mongoose.connect(process.env.MONGO_URI)// we use this to connect beacuse we dont want push this mongo_uri link to github for security thread
     .then(()=>{
         console.log("database is connected")
     })
