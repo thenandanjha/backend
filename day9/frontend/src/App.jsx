97641ed5e7ebd55d9ab4722a379265cc3615271a
Nandan Jha <nandanjha.dev@gmail.com>
2026-05-29 10:28:21 +0530

    dist folder updated

--- day9/frontend/src/App.jsx
+++ day9/frontend/src/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
+
+
 
 const App = () => {
   const [notes, setnotes] = useState([
@@ -19,18 +21,59 @@ const App = () => {
   }
 ])
 
- axios.get("http://localhost:3000/api/notes/")
-  .then((response)=>{
+  function fetchNotes(){
+    axios.get("https://backend-6vsn.onrender.com/")
+      .then((response)=>{
+        setnotes(response.data.notes)
+      })
+  }
+  function deleteHandle(noteId){
+   axios.delete("https://backend-6vsn.onrender.com/"+noteId)
+   .then(res=>{
+    fetchNotes()
+   })
+  }
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  function handleSubmit(e){
+   e.preventDefault()
+   const {title,description} =e.target.elements
+  //  console.log(title.value,description.value)
+
+   axios.post("https://backend-6vsn.onrender.com/",{
+    title:title.value,
+    description:description.value
+   })
+   .then((response)=>{
     // console.log(response.data)
-    setnotes(response.data.notes)
-  })
+    fetchNotes()
+   })
+  }
+  
   return (
     <>
+    <form className='note-create-form' onSubmit={(e)=>{
+      handleSubmit(e)
+    }}>
+      <input type="text" name="title" placeholder='Enter Title'/>
+      <input type="text" name="description" placeholder='Enter description' />
+      <button>Create Note</button>
+
+    </form>
+    
     <div className="notes">
       {notes.map((note,idx)=>{
         return <div className="note" key={idx}>
         <h1>{note.title}</h1>
         <p>{note.description}</p>
+        <button 
+        onClick={()=>{
+          deleteHandle(note._id) //note._id gives the value notes id creted by mongoose in databse
+        }}>
+          delete
+        </button>
       </div>
       })
       }
